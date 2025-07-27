320157ce8b29f9676e3c6e9c25609eea531d022d
SIMROZ RIYAZ <riyazsimroz0@gmail.com>
2025-07-27 21:51:34 +0900

    Add files via upload

---
+++ javasc2/day1.js
@@ -0,0 +1,7 @@
+//hello world
+
+alert ("Hello World!");
+
+// if statement
+let Javascript = "amazing"
+if(Javascript === "amazing") alert ("Javascript is FUN!");
---
+++ javasc2/day2.js
@@ -0,0 +1,39 @@
+
+                //switch statement
+
+                const day = 'tuesday';
+                switch (day) {
+                case 'monday':
+                    console.log('course');
+                    break;
+
+                    case 'tuesday':
+                    console.log('picture');
+                    console.log('videos');
+                    break;
+
+                    case 'wednesday':
+                      console.log('white');
+                      break;
+                      
+                      case 'thursday':
+                        console.log('black');
+                        break;
+
+                    case 'friday':
+                        console.log('red');
+                        break;
+
+                      case 'saturday':
+                        console.log('blue');
+                        break;
+                        
+                    case 'sunday':
+                        console.log('green');
+                        break;
+
+                        default:
+                        console.log('Not a valid day!');
+                }
+
+                        
